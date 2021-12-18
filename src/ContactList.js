7e15c49698b02b3a74b7dc03285ccd708c327fbc
Juan-Ceballos <juanceballos@pursuit.org>
2021-12-18 12:00:43 -0500

    added components

---
+++ src/ContactList.js
@@ -0,0 +1,21 @@
+// function createContact(name, age) {
+//     return {
+//         name,
+//         age
+//     };
+// }
+const ContactList = () => {
+    return (
+        <>
+            <p>Contacts</p>
+            <ul>
+                <li>Juan Ceballos, 28</li>
+                <li>Alex Paul, 30</li>
+                <li>Jacob Smith, 32</li>
+                <li>Susan Anthony, 25</li>
+            </ul>
+        </>
+    )
+}
+
+export default ContactList
